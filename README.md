5a397e4f4e2d5cd8d8774f7afadb9220e05e6d7a
israphial <israphial@users.noreply.github.com>
2020-12-27 23:01:48 -0500

    nightly: add JS function that generates cards, redo card image code

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Project 4: Around The U.S.
+# Project 4 and 5: Around The U.S.
 
 ### Overview
 
@@ -11,6 +11,8 @@
 Project 4 mixes HTML/CSS responsive design principles and Javascript to create a webpage that allows the client to edit certain elements of the profile. This is done via a popup window triggered via the edit button.
 The Nested BEM methodology is used to organize the filestructure, as well as the various entities that make up the page. 
 Additionally, the version history of this project may be messy, as I am using this project to experiment with and learn about git branches, merges, merge conflict resolution, and various other git conventions. My intent is to build features in branches that get merged with the branch called "develop", and the develop branch will periodically be merged into master at "stable" points in the process (upon each major revision of the project). The conventions I will be following are to prefix all branches with a title, such as feature/featureName, fix/fixName, etc. I will also be using imperative language for commits. 
+
+Project 5 expands on Project 4 by adding far more interactivity on the page. Cards are now more malleable, allowing the user to add their own custom cards, as well as remove existing cards. There are a few more animations added to various elements of the page, new functionality for the "like" buttons on each card, and a new popup that expands the picture of a card when clicked. Most of these additions will be done primarily through JavaScript DOM manipulation. At the moment, changes are not saved between reloads, as a backend server or DB has not been attached to the site. 
 
 **Technologies**
 
--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,7 @@
+// TODO: 
 
+
+/* proj4 code */
 let editButton = document.querySelector('.profile__edit');
 let closeButton = document.querySelector('.edit-form__close-button');
 let saveButton = document.querySelector('.edit-form__save-button');
@@ -34,3 +37,59 @@ function closePopupUnsaved() {
 editButton.addEventListener("click", openPopup);
 closeButton.addEventListener("click", closePopupUnsaved);
 popupForm.addEventListener("submit", closePopupSaved);
+
+/* End proj4 code */
+
+// TODO:
+// Make a function that loads all of the pre-made cards from the array of objects. First, make the bare elements that make up the card, then add the title, picture, and
+//  like button to each element. This could be accomplished using the <template> thing - clone the template for each card that needs to be made. maybe use the forEach method
+
+const initialCards = [
+    {
+      name: "Yosemite Valley",
+      link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+    },
+    {
+      name: "Lake Louise",
+      link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+    },
+    {
+      name: "Bald Mountains",
+      link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+    },
+    {
+      name: "Latemar",
+      link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+    },
+    {
+      name: "Vanoise National Park",
+      link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+    },
+    {
+      name: "Lago di Braies",
+      link: "https://code.s3.yandex.net/web-code/lago.jpg"
+    }
+  ];
+
+function generateCards() {
+    //for each object (card) in the initialCards array, clone the contents of the template and add the name and link to their title and img.src, respectively
+
+    const cardContainer = document.querySelector(".cards__container");
+    // clone the contents of the template 
+    const cardTemplate = document.querySelector("#card-template").content;
+
+
+    // 
+    initialCards.forEach(function (currentCardObject) {
+        const currentCard = cardTemplate.cloneNode(true);
+        // append the title and image src to the right elements
+        const currentCardImageSource = currentCard.querySelector(".card__picture").src = currentCardObject.link;
+        currentCardImageAltText = currentCard.querySelector(".card__picture").alt = currentCardObject.name;
+        const currentCardTitleText = currentCard.querySelector(".card__name").textContent = currentCardObject.name;
+
+        // append currentCard to cardContainer
+        cardContainer.append(currentCard);
+    })
+
+}
+
